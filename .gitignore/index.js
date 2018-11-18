b2fc4d6492fdad7f69a72e87766819372eebed17
May0000 <45133505+May0000@users.noreply.github.com>
2018-11-18 01:24:14 +0100

    Create index.js

---
+++ .gitignore/index.js
@@ -0,0 +1,281 @@
+const Discord = require('discord.js');
+
+const client = new Discord.Client();
+
+var prefix = "*";
+
+const ytdl = require('ytdl-core');
+
+const ffmpeg = require('ffmpeg-binaries');
+
+const queue = new Map();
+//------------------------------//------------------------------//------------------------------//------------------------------//
+
+//------------------------------//------------------------------//------------------------------//------------------------------//
+var servers = {};
+//-----------------------------------------------------------------------------------------------------------//
+client.login(process.env.TOKEN)
+//-----------------------------------------------------------------------------------------------------------//
+function play(connection, message) {
+
+  var server = servers[message.guild.id];
+
+  server.dispatcher = connection.playStream(ytdl(server.queue[0], {filter: "audioonly"}));
+
+  server.queue.shift();
+
+  server.dispatcher.on("end", function() { 
+    if (server.queue[0]) play(connection, message);
+
+    else connection.disconnect();
+
+  });
+}
+//-----------------------------------------------------------------------------------------------------------//
+client.on("ready", () => {
+
+    console.log("Je suis prêt !");
+    client.user.setActivity("COMMANDES *sotuhelp");
+
+});
+
+client.on('message', async message => { 
+
+    if(message.content === "Bonjour"){
+        message.reply("Salut");
+        console.log('Le bot dit bonjour');
+    }
+//-----------------------------------------------------------------------------------------------------------//
+    if(message.content === prefix + "sotuhelp") {
+      var aide_embed = new Discord.RichEmbed()
+      .setColor('RANDOM')
+      .setTitle(`:robot: Voici mes catégories d'aide !`)
+      .setDescription(`Voici mes commandes disponible :`)
+      .setThumbnail(message.author.avatarURL)
+      .addField(":tools: Modération", "Fais `*sotumod` pour voir mes commandes de modération !")
+      .addField(":tada: Fun", "Fais `*sotufun` pour voir mes commandes d'animation !")
+      .setFooter("Menu d'aide - Sotura")
+      .setTimestamp()
+      message.channel.send(aide_embed);
+    }
+//-----------------------------------------------------------------------------------------------------------//
+    if(message.content === prefix + "sotumod") {
+      var mod_embed = new Discord.RichEmbed()
+      .setColor('RANDOM')
+      .setTitle(`:tools: Voici mes commandes modérations !`)
+      .setThumbnail(message.author.avatarURL)
+      .addField("*sotukick <@user>", "Kick l'utilisateur !")
+      .addField("*sotuban <@user>", "Ban l'utilisateur !")
+      .addField("*sotuclear nombre", "Supprime le nombre de messages indiqué")
+      .addField("*sotumute <@user>", "Mute l'utilisateur mentionné")
+      .addField("*sotuunmute <@user>", "Unmute l'utilisateur mentionné")
+      .setFooter("Commande modération - Sotura")
+      .setTimestamp()
+      message.channel.send(mod_embed);
+    }
+//-----------------------------------------------------------------------------------------------------------//
+    if(message.content === prefix + "sotufun") {
+      var fun_embed = new Discord.RichEmbed()
+      .setColor('RANDOM')
+      .setTitle(`:tools: Voici mes commandes amusantes !`)
+      .setThumbnail(message.author.avatarURL)
+      .addField("Bonjour", "Le bot répond !")
+      .addField("*stats", "Le bot vous envoie des informations sur votre profil !")
+      .addField("*info", "Donne des indormations sur le bot et le serveur !")
+      .setFooter("Commande Fun - Sotura")
+      .setTimestamp()
+      message.channel.send(fun_embed);
+    }
+//-----------------------------------------------------------------------------------------------------------//
+    if (!message.content.startsWith(prefix)) return;
+
+    var args = message.content.substring(prefix.length).split(" ");
+
+    switch (args[0].toLowerCase()) { 
+
+        case "sotustats":
+
+        var userCreateDate = message.author.createdAt.toString().split(" ");
+        var msgauthor = message.author.id;
+
+        var stats_embed = new Discord.RichEmbed()
+        .setColor("#6699FF")
+        .setTitle(`Statistiques du joueurs : ${message.author.username}`)
+        .addField(`ID du joueurs :id:`, msgauthor, true)
+        .addField(`Date d'inscription du joueur :`, userCreateDate[1] + ' ' + userCreateDate[2] + ' ' + userCreateDate[3])
+        .setThumbnail(message.author.avatarURL)
+        message.reply("Tu peux regarder tes messages privés !")
+        message.author.send(stats_embed);
+
+        break;
+//-----------------------------------------------------------------------------------------------------------//  
+case "play":
+
+    if (!args[1]) {
+
+    message.channel.sendMessage("Tu dois m’indiquer un lien YouTube"); 
+
+    return;
+
+  }
+
+    if(!message.member.voiceChannel) {
+
+    message.channel.sendMessage(":x: Tu dois être dans un salon vocal"); 
+
+    return;
+
+  }
+
+
+    if(!servers[message.guild.id]) servers[message.guild.id] = {
+
+    queue: []
+
+  };
+
+
+  var server = servers[message.guild.id];
+
+
+  server.queue.push(args[1]);
+
+  if(!message.guild.voiceConnection) message.member.voiceChannel.join().then(function(connection) {
+
+  play(connection, message) 
+
+  });
+
+  break; 
+
+  case "skip":
+
+    if(!message.member.voiceChannel) {
+
+    message.channel.sendMessage(":x: Tu dois être dans un salon vocal");
+
+    return;
+
+  }
+
+    var server = servers[message.guild.id];
+
+    if(server.dispatcher) server.dispatcher.end();
+
+    break;
+
+  case "stop":
+
+    if(!message.member.voiceChannel) 
+    
+    return message.channel.send(":x: Tu dois être dans un salon vocal");
+
+    message.member.voiceChannel.leave();
+
+    break;
+  
+  }
+//-----------------------------------------------------------------------------------------------------------//
+    if(message.content === prefix + "sotuinfo") {
+        var info_embed = new Discord.RichEmbed()
+        .setColor("#40A497")
+        .setTitle("Voici les informations sur moi et le serveur !")
+        .addField(" :robot: Nom :", `${client.user.tag}`, true)
+        .addField("Descriminateur du bot :hash:", `#${client.user.discriminator}`)
+        .addField("ID :id: ", `${client.user.id}`)
+        .addField("Nombre de membres", message.guild.members.size)
+        .addField("Nombre de catégories et de salons", message.guild.channels.size)
+        .setFooter("Informations - Sotura")
+        message.channel.sendMessage(info_embed)
+        console.log("Un utilisateur a effectué la commande d'info !")
+    }
+//-----------------------------------------------------------------------------------------------------------//
+    if(message.content.startsWith(prefix + "sotukick")){
+        if(!message.guild.member(message.author).hasPermission("KICK_MEMBERS")) return message.channel.send("Vous n'avez pas la permission!");
+    
+        if(message.mentions.users.size === 0) {
+            return message.channel.send("Vous devez metionner un utilisaeur")
+        }
+        var kick = message.guild.member(message.mentions.users.first());
+        if(!kick) {
+            return message.channel.send("Je ne sais pas si l'utilisateur existe :/")
+        }
+    
+        if(message.guild.member(client.user).hasPermission("KICK_MEMBERS")) {
+            return message.channel.send("Je n'ai pas la permission pour kick");
+        }
+    
+        kick.kick().then(member => {
+            message.channel.send(`${member.user.username} est kick pas ${message.author.username}`);
+        });
+    }
+//-----------------------------------------------------------------------------------------------------------//
+    if(message.content.startsWith(prefix + "sotuban")) {
+        if(!message.guild.member(message.author).hasPermission("BAN_MEMBERS")) return message.channel.send("Vous n'avez pas la perission");
+
+        if(message.mentions.users.size === 0) {
+            return message.channel.send("Vous devez mentionner un utilisateur");
+        }
+
+        var ban = message.guild.member(message.mentions.users.first());
+        if(!ban) {
+            return message.channel.send("Je ne sais pas si l'utilisateur existe");
+        }
+
+        if(!message.guild.member(client.user).hasPermission("BAN_MEMBERS")) {
+            return message.channel.send("Je n'ai pas la permission pour ban");
+        }
+        ban.ban().then(member => {
+            message.channel.send(`${member.user.username} est ban par ${message.author.username} !`)
+        });
+        
+    }
+//-----------------------------------------------------------------------------------------------------------//
+    if(message.content.startsWith(prefix + "sotuclear")) {
+        if(!message.guild.member(message.author).hasPermission("MANAGE_MESSAGE")) return message.channel.send("Vous n'avez pas la permission !");
+
+        let args = message.content.split(" ").slice(1);
+
+        if(!args[0]) return message.channel.send("Tu dois préciser un nombre de messages à supprimer !")
+        message.channel.bulkDelete(args[0]).then(() => {
+            message.channel.send(`${args[0]} messages ont été supprimés !`);
+        });
+    }
+//-----------------------------------------------------------------------------------------------------------//
+    if(message.content.startsWith(prefix + "sotumute")) {
+        if(!message.guild.member(message.author).hasPermission("ADMINISTRATOR")) return message.channel.send("Vous n'avez pas la permission !");
+
+        if(message.mentions.users.size === 0) {
+            return message.channel.send('Vous devez mentionner un utilisateur !');
+        }
+
+        var mute = message.guild.member(message.mentions.users.first());
+        if(!mute) {
+            return message.channel.send("Je n'ai pas trouvé l'utilisateur ou il l'existe pas !");
+        }
+
+        if(!message.guild.member(client.user).hasPermission("ADMINISTRATOR")) return message.channel.send("Je n'ai pas la permission !");
+        message.channel.overwritePermissions(mute, { SEND_MESSAGES: false}).then(member => {
+            message.channel.send(`${mute.user.username} est mute !`);
+        });
+    }
+//-----------------------------------------------------------------------------------------------------------//
+    if(message.content.startsWith(prefix + "sotuunmute")) {
+        if(!message.guild.member(message.author).hasPermission("ADMINISTRATOR")) return message.channel.send("Vous n'avez pas la permission !");
+
+        if(message.mentions.users.size === 0) {
+            return message.channel.send('Vous devez mentionner un utilisateur !');
+        }
+
+        var mute = message.guild.member(message.mentions.users.first());
+        if(!mute) {
+            return message.channel.send("Je n'ai pas trouvé l'utilisateur ou il l'existe pas !");
+        }
+
+        if(!message.guild.member(client.user).hasPermission("ADMINISTRATOR")) return message.channel.send("Je n'ai pas la permission !");
+        message.channel.overwritePermissions(mute, { SEND_MESSAGES: true}).then(member => {
+            message.channel.send(`${mute.user.username} n'est plus mute !`);
+        })
+    }
+});
+//-----------------------------------------------------------------------------------------------------------//
